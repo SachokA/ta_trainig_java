39b1db33114860c147620f9cc50958e548301dcb
SachokA <agukavchyk+10@gmail.com>
2024-08-15 19:52:03 +0300

    remove impl

--- src/test/java/com/epam/training/AlionaSachok/Task3/BaseTest.java
+++ src/test/java/com/epam/training/AlionaSachok/Task3/BaseTest.java
@@ -17,18 +17,12 @@ public class BaseTest {
 
     private static final String BASE_URL = "https://cloud.google.com/";
 
-    public void waitWebElementVisibilityOf(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOf(element));
-    }
-
     @BeforeEach
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get(BASE_URL);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
     @AfterEach
